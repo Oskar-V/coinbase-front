380944d099e43be505a09ef8fa8f83bed490c16c
ktaal <karltaal@gmail.com>
2022-01-16 14:50:28 +0200

    data update mechanism

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 import './App.css';
 import ProductMenu from "./components/ProductMenu/ProductMenu";
 import ProductCandleChart from "./components/ProductCandleChart/ProductCandleChart";
 import {SnackbarProvider} from 'notistack';
+import Header from "./components/Header/Header";
 
 function App() {
   const [selectedProductsIdList, setSelectedProductsIdList] = useState<string[]>([]);
+  const [dataUpdateCount, setDataUpdateCount] = useState<number>(0);
 
   return (
     <SnackbarProvider>
-      <div className="App">
+      <Header dataUpdateCountHandler={() => {
+        setDataUpdateCount(dataUpdateCount + 1)
+      }}/>
+      <div className={"app-body"}>
         <ProductMenu productSelectHandler={setSelectedProductsIdList}/>
         <div>
-          {selectedProductsIdList.map(id => <ProductCandleChart key={id} productId={id}/>)}
+          {selectedProductsIdList.map(id => <ProductCandleChart key={id} productId={id} dataUpdateCount={dataUpdateCount}/>)}
         </div>
       </div>
     </SnackbarProvider>
